71d36a25f278775ea94a10a0ee568b9521f21dd3
carlossierrafy24 <carlossierrafy@gmail.com>
2025-05-18 10:59:22 -0500

    feat(frontend): refactor App component to integrate Google Maps API and remove unused code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { LoadScript } from "@react-google-maps/api";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <LoadScript
+      googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+      libraries={["places"]}
+      language="en"
+      region="US"
+      version="weekly"
+    >
+      <App />
+    </LoadScript>
+  </StrictMode>
+);
