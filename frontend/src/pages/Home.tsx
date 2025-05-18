82ae3a66d370ed071b80a67f0ee3be40133ecd31
carlossierrafy24 <carlossierrafy@gmail.com>
2025-05-18 10:20:34 -0500

    feat(frontend): add Home component wrapper for CityForm component

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { CityForm } from "../components/CityForm";
+
+type Props = {};
+
+function Home({}: Props) {
+  return (
+    <div>
+      <CityForm />
+    </div>
+  );
+}
+
+export default Home;
